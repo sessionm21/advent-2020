2911a7e2428248cb65f08e5431bc5e37642be908
sessionm21 <sessionm@oregonstate.edu>
2020-12-01 16:39:00 -0800

    cleanup

--- day1.lisp
+++ day1.lisp
@@ -2,15 +2,15 @@
 (ql:quickload "split-sequence")
 (ql:quickload "uiop")
 
-
-(let ((lines (uiop:read-file-lines "input.txt")))
-  (defun parseInts(strings)
-    (loop for x in strings collect (read-from-string x)))
-  (defun makeSum()
+(defun parseInts(strings)
+  (loop for x in strings collect (read-from-string x)))
+(defun makeSum()
+  (let ((lines (uiop:read-file-lines "input.txt")))
     (let ((nums (parseInts lines)))
       (loop for x in nums do
         (loop for y in nums do
           (when (= (+ x y) 2020) (return-from makeSum (* x y)))))
       )
     )
-  (print (makeSum)))
+  )
+(print (makeSum))
