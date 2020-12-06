75e8db0b8cd6c74c9f719799fb5d573edaeab54d
sessionm21 <sessionm@oregonstate.edu>
2020-12-05 22:31:12 -0800

    day 6

--- day1.lisp
+++ day1.lisp
@@ -4,13 +4,23 @@
 
 (defun parseInts(strings)
   (loop for x in strings collect (read-from-string x)))
-(defun makeSum()
-  (let ((lines (uiop:read-file-lines "input.txt")))
+;; (defun partOne()
+;;   (let ((lines (uiop:read-file-lines "input1.txt")))
+;;     (let ((nums (parseInts lines)))
+;;       (setq nums (sort nums '<))
+;;       (loop for x in nums do
+;;         (loop for y in nums do
+;;             (when (= (+ x y) 2020) (return-from partOne (* x y))))))
+;;     )
+;;   )
+(defun partTwo()
+  (let ((lines (uiop:read-file-lines "input1.txt")))
     (let ((nums (parseInts lines)))
+      (setq nums (sort nums '<))
       (loop for x in nums do
         (loop for y in nums do
-          (when (= (+ x y) 2020) (return-from makeSum (* x y)))))
-      )
+          (loop for z in nums do
+            (when (= (+ (+ x y) z) 2020) (return-from partTwo (* (* x y) z)))))))
     )
   )
-(print (makeSum))
+(print (partTwo))
