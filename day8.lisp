eca0fde02099e62ccc441de1e0bfff81c8ba1340
Matt <user@fed-desk.hsd1.or.comcast.net>
2020-12-08 13:07:28 -0800

    cleanup

--- day8.lisp
+++ day8.lisp
@@ -9,46 +9,56 @@
   (ql:quickload 'uiop)
   (ql:quickload 'cl-ppcre))
 
-(defun system(lines)
-  (let ((original-instructions (cl-ppcre:split "\\n" (cl-ppcre:regex-replace-all "\\n\\n" lines "")))
-        (accumulator 0))
-    (defun nop(arg) 1)
-    (defun acc(arg) (setq accumulator (+ accumulator arg)) 1)
-    (defun jmp(arg) arg)
-    (defun custom-test(a b)
-      (destructuring-bind (n c) a
-        (destructuring-bind (_n _c) b
-          (and (= n _n) (string= c _c)))))
-    (defun is-jump(a)
-      (cl-ppcre:all-matches-as-strings "jmp" a))
-    (defun _run-program(instructions)
-      (let ((line-number 0) (lines-run (list (list 0 "START"))) (running T))
-        (setq accumulator 0)
-        (loop while running do
-          (let ((line (nth line-number instructions)))
-            (if (or (member (list line-number line) lines-run :test #'custom-test))
-                (progn ; kill loop
-                  (setq lines-run (append lines-run (list (list line-number "kill +0"))))
-                  (setq running Nil))
-                (progn  ; normal operation
-                  (setq lines-run (append lines-run (list (list line-number line))))
-                  (setq line-number (+ line-number ;; move from line number (jump)
-                                       (eval ;; call string directly
-                                        (car (read-from-string (concatenate 'string "((" line "))"))))))))
-            (if (>= line-number (length instructions)) (setq running Nil))))
-        (list lines-run accumulator)))
+(defun my-parser(str)
+  (let* ((split-regex " ")
+         (replace-regex "")
+         (replace-to "")
+         (replaced (cl-ppcre:regex-replace-all replace-regex str replace-to)))
+    (cl-ppcre:split split-regex replaced)))
 
-    (destructuring-bind (lines-run acc) (_run-program original-instructions)
-      (print (list "part one:" acc))
-      (loop for (n i) in lines-run
-            when (is-jump i) ; part two
-              do
-                 (let ((instructions (copy-list original-instructions)))
-                   (setf (nth n instructions) "nop +0")
-                   (destructuring-bind (lines-run acc) (_run-program instructions)
-                     (destructuring-bind (n op) (nth (- (length lines-run) 1) lines-run)
-                       (if (not (string= op "kill +0"))
-                           (print (list "part two: " acc))))))))))
+(defun parse-file(parser file-name)
+  (loop for line in (uiop:read-file-lines file-name)
+        collect (funcall parser line)))
 
-(let ((lines (uiop:read-file-string "input.txt")))
-  (system lines)) ; part one and two
+(defun system(original-instructions)
+  (defun nop(arg) (list 1 0))
+  (defun acc(arg) (list 1 arg))
+  (defun jmp(arg) (list arg 0))
+  (defun custom-test(a b)
+    (cond ((listp b)
+           (destructuring-bind ((_an (_aop _aarg)) (_bn (_bop _barg)))
+               (list a b)
+             (and (= _an _bn) (string= _aop _bop) (string= _aarg _barg))))))
+  (defun _run-program(lines-run line-number accumulator instructions)
+    (let* ((line (nth line-number instructions))
+           (op (nth 0 line))
+           (arg (nth 1 line)))
+      (if (>= line-number (length instructions))
+          (list lines-run accumulator)
+          (if (member (list line-number line) lines-run :test #'custom-test)
+              (list (append lines-run (list (list line-number (list "kill" "0")))) accumulator)
+              (progn
+                (destructuring-bind (_jmp _acc)
+                    (funcall (read-from-string op) (read-from-string arg))
+                  (_run-program
+                   (append lines-run (list (list line-number line)))
+                   (+ line-number _jmp)
+                   (+ accumulator _acc)
+                   instructions)))))))
+  (destructuring-bind (lines-run acc)
+      (_run-program Nil 0 0 original-instructions)
+    (list "part one:" acc "part two:"
+          (remove-duplicates
+           (loop for (n (op arg)) in lines-run
+                 when (string= op "jmp")
+                   collect (let ((instructions (copy-list original-instructions)))
+                             (setf (nth n instructions) (list "nop" "0"))
+                             (destructuring-bind (lines-run _acc)
+                                 (_run-program (list 0 "START") 0 0 instructions)
+                               (destructuring-bind (n (op arg))
+                                   (nth (- (length lines-run) 1) lines-run)
+                                 (if (not (string= op "kill"))
+                                     _acc)))))))))
+
+(print (let ((parsed (parse-file 'my-parser "input.txt")))
+         (system parsed)))
