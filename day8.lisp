b533c88ad7189ee09729c4355dc4e02643ab8cae
Matt <user@fed-desk.hsd1.or.comcast.net>
2020-12-07 23:39:13 -0800

    cleanup

--- day8.lisp
+++ day8.lisp
@@ -11,10 +11,7 @@
 
 (defun system(lines)
   (let ((original-instructions (cl-ppcre:split "\\n" (cl-ppcre:regex-replace-all "\\n\\n" lines "")))
-        (running T)
-        (line-number 0)
-        (accumulator 0)
-        (lines-run (list (list 0 "START"))))
+        (accumulator 0))
     (defun nop(arg) 1)
     (defun acc(arg) (setq accumulator (+ accumulator arg)) 1)
     (defun jmp(arg) arg)
@@ -25,35 +22,33 @@
     (defun is-jump(a)
       (cl-ppcre:all-matches-as-strings "jmp" a))
     (defun do-loop(instructions)
-      (setq lines-run (list (list 0 "START")))
-      (setq line-number 0)
-      (setq running T)
-      (setq accumulator 0)
-      (loop while running do
-        (let ((line (nth line-number instructions)))
-          (if (or (member (list line-number line) lines-run :test #'custom-test))
-              (progn ; kill loop
-                (setq lines-run (append lines-run (list (list line-number "kill +0"))))
-                (setq running Nil))
-              (progn  ; normal operation
-                (setq lines-run (append lines-run (list (list line-number line))))
-                (setq line-number (+ line-number (eval (car (read-from-string (concatenate 'string "((" line "))"))))))))
-          (if (>= line-number (length instructions)) (setq running Nil)))))
+      (let ((line-number 0) (lines-run (list (list 0 "START"))) (running T))
+        (setq accumulator 0)
+        (loop while running do
+          (let ((line (nth line-number instructions)))
+            (if (or (member (list line-number line) lines-run :test #'custom-test))
+                (progn ; kill loop
+                  (setq lines-run (append lines-run (list (list line-number "kill +0"))))
+                  (setq running Nil))
+                (progn  ; normal operation
+                  (setq lines-run (append lines-run (list (list line-number line))))
+                  (setq line-number (+ line-number ;; move from line number (jump)
+                                       (eval ;; call string directly
+                                        (car (read-from-string (concatenate 'string "((" line "))"))))))))
+            (if (>= line-number (length instructions)) (setq running Nil))))
+        (list lines-run accumulator)))
 
-    (do-loop original-instructions) ; part one
-    (loop for (n i) in lines-run
-          when (is-jump i) ; part two
-            do
-               (print (list "SETTING:" n i))
-               (let ((instructions (copy-list original-instructions)))
-                 (setf (nth n instructions) "nop +0")
-                 (do-loop instructions)
-                 (print (nth (- (length lines-run) 1 ) lines-run))
-                 (print accumulator)
-                 (print "==================================")))
-
-    (do-loop original-instructions) ; part one
-    (print (list "ACCUMULATOR: " accumulator))))
+    (destructuring-bind (lines-run acc) (do-loop original-instructions)
+      (print (list "part one:" acc))
+      (loop for (n i) in lines-run
+            when (is-jump i) ; part two
+              do
+                 (let ((instructions (copy-list original-instructions)))
+                   (setf (nth n instructions) "nop +0")
+                   (destructuring-bind (lines-run acc) (do-loop instructions)
+                     (destructuring-bind (n op) (nth (- (length lines-run) 1) lines-run)
+                       (if (not (string= op "kill +0"))
+                           (print (list "part two: " acc))))))))))
 
 (let ((lines (uiop:read-file-string "input.txt")))
   (system lines)) ; part one and two
