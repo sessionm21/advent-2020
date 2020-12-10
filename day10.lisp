3e69346c0fa980ae95db11d2316b57d541921b3b
Matt <user@fed-desk.hsd1.or.comcast.net>
2020-12-10 13:30:09 -0800

    cleanup

--- day10.lisp
+++ day10.lisp
@@ -22,72 +22,64 @@
          (replace-regex "")
          (replace-to "")
          (replaced (cl-ppcre:regex-replace-all replace-regex str replace-to)))
-    ;(cl-ppcre:split split-regex replaced))
+    ;; (cl-ppcre:split split-regex replaced))
     (parse-integer str)))
 
 (defun parse-file(parser file-name)
   (loop for line in (uiop:read-file-lines file-name)
         collect (funcall parser line)))
 
-(defun recursive-solve(element ls built-in)
-  (if (and ls (> (length ls) 0))
-      (let ((original (copy-list ls))
-            (first (pop ls)))
-        (if (<=(abs (- first element)) 3)
-            (append (list (- first element)) (recursive-solve first ls built-in))
-            (let ((second (pop ls)))
-              (if (<=(abs (- second element)) 3)
-                  (append (list (- second element)) (recursive-solve second ls built-in)))
-              (let ((third (pop ls)))
-                (if (<=(abs (- third element)) 3)
-                    (append (list (- third element)) (recursive-solve third ls built-in))
-                    )))))
-      (list (- built-in element))
-      ))
-
-
 (defun partOne(parsed)
+  (defun recursive-solve(element ls built-in)
+    (if (and ls (> (length ls) 0))
+        ;; pull from list of elements
+        (let ((original (copy-list ls))
+              (first element))
+          (loop for i in (list 0 1 2)
+                for el in ls
+                while (<= (abs (- first el)) 3)
+                collect
+                (let ((e (pop ls)))
+                  (return (append (list (- e first)) (recursive-solve e ls built-in))))))
+        ;; final element
+        (list (- built-in element))))
+
   (let* ((sorted (sort parsed #'<))
          (last (nth (- (length sorted) 1) sorted))
          (built-in (+ last 3)))
-    (print sorted)
     (let* ((original-sol (sort (recursive-solve 0 sorted built-in) #'<))
-           (no-dupes (remove-duplicates original-sol)))
-      (print (apply #'* (loop for e in no-dupes collect (length  (loop for x in original-sol when (= x e) collect x)))))
-      (print original-sol)
-      (print no-dupes)
-      )
-  ))
+           (no-dupes (remove-duplicates original-sol))
+           (rows (loop for e in no-dupes collect (list e (length  (loop for x in original-sol when (= x e) collect x)))))
+           (multiplied (apply #'* (loop for (n e) in rows collect e))))
+      ;; results
+      (list "rows x y:" rows
+            "multiplied:" multiplied))))
 
 
 (defparameter my-hash (make-hash-table))
+
 (defun partTwo(parsed)
   (defun recursive-solve-two(element ls built-in)
     (if (<= (abs (- element built-in)) 3)
-        (progn
-          (setf (gethash element my-hash) 1)
-          1)
-        (progn
-          (let ((query (gethash element my-hash)))
-            (if query
-                (progn
-                  query)
-                (let* ((copied ls)
-                       (res (apply #'+ (loop for i in (list 0 1 2)
-                                                  if (and copied (and (<= (abs (- (nth 0 copied) element)) 3)))
-                                                    collect (let ((x (pop copied)))
-                                                              (if (and (<= (abs (- x element)) 3))
-                                                                  (recursive-solve-two x copied built-in)
-                                                                  ))))))
-                  (setf (gethash element my-hash) res)
-                  res))))))
-
+        (setf (gethash element my-hash) 1)
+        (apply #'+ (loop for i in (list 0 1 2)
+                         if (and ls (and (<= (abs (- (nth 0 ls) element)) 3)))
+                           collect (recursive-solve-two (pop ls) ls built-in)))))
+  ;; define memoize for 3 arguments that caches to the hash table
+  (defun memoize(f)
+    #'(lambda (a &rest args)
+        (let ((query (gethash a my-hash)))
+          (if query
+              query
+              (setf (gethash a my-hash) (funcall f a (nth 0 args) (nth 1 args)))))))
+  ;; memoize the function
+  (setf (fdefinition 'recursive-solve-two) (memoize #'recursive-solve-two))
+  ;; set up and call
   (let* ((sorted (sort parsed #'<))
          (last (nth (- (length sorted) 1) sorted))
          (built-in (+ last 3))
          (res (recursive-solve-two 0 sorted built-in)))
-    res
-    ))
+    res))
 
 
 (let ((parsed (parse-file 'my-parser "input10.txt")))
